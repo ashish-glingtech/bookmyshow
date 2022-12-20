48470040c3de6c13f6ba7451fb586ebdaa1c24b4
ankur1singh <ankur.singh@glingtech.com>
2022-12-20 17:46:19 +0530

    update files

--- app.py
+++ app.py
@@ -48,72 +48,71 @@ from models import db, User, Movie, Theater, Screen, Booking, Payment
 
 db.init_app(app)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods = ['POST'])
 def create_login():
   body_data=request.get_json()
   #result.append(body_data)
-  user=User(name=body_data['name'], gender=body_data['gender'], age=body_data['age'], email=body_data['email'], phone_no=body_data['phone_no'])
+  user=User(name = body_data['name'], gender = body_data['gender'], age = body_data['age'], email = body_data['email'], phone_no = body_data['phone_no'])
   db.session.add(user)
   db.session.commit()
  
   print(body_data['name'])
 
-  return jsonify({"message":"User create succesfull"})
+  return jsonify({"message" : "User create succesfull"})
 
-@app.route('/movies', methods=['POST'])
+@app.route('/movies', methods = ['POST'])
 def add_movie():
   data = request.get_json()
-  movie=Movie(name=data['name'],  descr =data['descr'], duration=data['duration'],  language=data['language'], movie_type=data['movie_type'])
+  movie=Movie(name = data['name'], descr = data['descr'], duration = data['duration'], language = data['language'], movie_type = data['movie_type'])
 
   db.session.add(movie)
   db.session.commit()
  
-  return jsonify({"message":"Movies Add succesfull"})
+  return jsonify({"message" : "Movies Add succesfull"})
 
 
 
-@app.route('/theater', methods=['POST'])
+@app.route('/theater', methods = ['POST'])
 def add_theater():
   data = request.get_json()
-  theater=Theater(name=data['name'],location =data['location'],rating=data['rating'],phone_no=data['phone_no'])
+  theater=Theater(name = data['name'], location = data['location'], rating = data['rating'], phone_no = data['phone_no'])
   
   db.session.add(theater)
   db.session.commit()
  
-  return jsonify({"message":"Theater Add succesfull"})
+  return jsonify({"message" : "Theater Add succesfull"})
 
 
-@app.route('/screen', methods=['POST'])
+@app.route('/screen', methods = ['POST'])
 def add_screen():
   data = request.get_json()
-  screen=Screen(name=data['name'], movie_id =data['movie_id'],theater_id=data['theater_id'],ticket_type=data['ticket_type'],total_seats=data['total_seats'])
+  screen=Screen(name = data['name'], movie_id = data['movie_id'], theater_id = data['theater_id'], ticket_type = data['ticket_type'], total_seats = data['total_seats'])
 
   db.session.add(screen)
   db.session.commit()
  
-  return jsonify({"message":"Screen Add succesfull"})
+  return jsonify({"message" : "Screen Add succesfull"})
 
 
-@app.route('/booking', methods=['POST'])
+@app.route('/booking', methods = ['POST'])
 def add_booking():
   data = request.get_json()
-  booking=Booking(screen_id=data['screen_id'],user_id =data['user_id'],booking_no=data['booking_no'],date =data['date'],start_time=data['start_time'],end_time=data['end_time'],payment=data['payment'],status=data['status'])
-  print(booking)
+  booking=Booking(screen_id = data['screen_id'], user_id = data['user_id'], booking_no = data['booking_no'], date = data['date'], start_time = data['start_time'], end_time = data['end_time'], payment = data['payment'], status = data['status'])
   db.session.add(booking)
   db.session.commit()
  
-  return jsonify({"message":"Booking Add succesfull"})
+  return jsonify({"message" : "Booking Add succesfull"})
 
 
-@app.route('/payment', methods=['POST'])
+@app.route('/payment', methods = ['POST'])
 def add_payment():
   data = request.get_json()
-  payment=Payment(payment_type =data['payment_type'],booking_id =data['booking_id'],user_id=data['user_id'],amount =data['amount'],date=data['date'])
-  print(payment)
+  payment=Payment(payment_type = data['payment_type'], booking_id = data['booking_id'], user_id = data['user_id'], amount = data['amount'], date = data['date'])
+
   db.session.add(payment)
   db.session.commit()
  
-  return jsonify({"message":"Payment Add succesfull"})
+  return jsonify({"message" : "Payment Add succesfull"})
 
 
 if __name__ == "__main__":
